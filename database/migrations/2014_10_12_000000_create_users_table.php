16b97ece7565ec3d0c54ec5ad1e44701f431cc02
ericivan <ericivan047@gmail.com>
2018-04-12 21:31:02 +0800

    fix user migrateion

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->integer('gold', false, true);
             $table->unique('email');
         });
     }
