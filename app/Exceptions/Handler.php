8c7eff389a08dadcd33490b809c14f5fe924d348
ericivan <ericivan047@gmail.com>
2018-04-13 09:55:52 +0800

    add laravel 5.5 test

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,7 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 
 class Handler extends ExceptionHandler
 {
@@ -61,5 +62,12 @@ class Handler extends ExceptionHandler
         }
 
         return redirect()->guest('login');
+    }
+
+    protected function invalidJson($request, ValidationException $exception)
+    {
+        return response()->json([
+            'msg' => $exception->getMessage(),
+        ]);
     }
 }
